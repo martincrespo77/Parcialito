b733edd22a969cdb1780ea72c2dcf13725834ade
martincrespo77 <mcrespo@fie.undef.edu.ar>
2023-03-31 12:56:01 -0300

    Main

--- main.c
+++ main.c
@@ -4,12 +4,13 @@ int main() {
     float nota,sumanota=0, totalnota=0;
     while(nota!=-1){
         printf("Ingrese las notas del alumno, si desea salir del programa colocar un | -1 |  \n");
-        scanf("%f",&nota);
-        printf("nota %.2f \n",nota);
+        scanf("%f",& nota);
+        //printf("valor agregado es2 %.2f \n",nota);
+        if(nota!=-1 || nota<0){
         totalnota++;
-        sumanota += nota;
+        sumanota += nota;}
     }
-    totalnota= totalnota -1;
+
     printf("El promedio es: %.2f\n",sumanota/totalnota);
 
     return 0;
