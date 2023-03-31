db5c0278e9bbb2623ea0ab83ea824f9214b5ea34
martincrespo77 <mcrespo@fie.undef.edu.ar>
2023-03-31 10:40:47 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float nota,sumanota=0, totalnota=0;
+    while(nota!=-1){
+        printf("Ingrese las notas del alumno, si desea salir del programa colocar la un | -1 |  \n");
+        scanf("%f",&nota);
+        printf("nota %.2f \n",nota);
+        totalnota++;
+        sumanota +=  nota;
+    }
+    totalnota= totalnota;
+    printf("El promedio es: %.2f\n",sumanota/totalnota);
+    printf("El nota es: %.2f\n",totalnota);
+    printf("El nota es: %.2f\n",sumanota);
+    return 0;
+}
+
